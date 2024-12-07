e2eeabecc54b01745b17a9c1c9901513ba277b82
joaopedrovidal <ppvidalsantos@gmail.com>
2024-12-07 20:19:01 -0300

    Inserindo comentários no Array

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,9 +8,10 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }){
     const [comments, setComments] = useState([
-        1,
-        2,
+        "Muito bom!"
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedAtDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -23,7 +24,14 @@ export function Post({ author, publishedAt, content }){
 
     function handleCreateNewComment (){
         event.preventDefault()
-        setComments([...comments, comments.length + 1])
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('');
+
+    }
+
+    function handleNewCommentChange (){
+        setNewCommentText(event.target.value);
     }
 
     return(
@@ -56,7 +64,10 @@ export function Post({ author, publishedAt, content }){
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -66,7 +77,7 @@ export function Post({ author, publishedAt, content }){
 
             <div className={styles.commentList}>
                 {comments.map(comment =>{
-                    return <Comment />
+                    return <Comment content={comment}/>
                 })}
             </div>
 
