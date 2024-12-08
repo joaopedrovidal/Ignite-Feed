eebe0d16a8fc08d32e41db50416a87c21bf1d94d
joaopedrovidal <ppvidalsantos@gmail.com>
2024-12-07 22:45:49 -0300

    Comunicação entre componentes (deletando comentário)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({content}){
+export function Comment({content, onDeleteComment}){
+    function handleDeleteComment(){
+        console.log('deletar')
+        onDeleteComment(content);
+    }
+
     return (
       <div className={styles.comment}>
         <Avatar hasBorder={false} src="https://github.com/joaopedrovidal.png" />
@@ -15,7 +20,7 @@ export function Comment({content}){
                         <time title="07 de Dezembro às 15:18" datetime="2024-07-12 15:19:30">Cerca de 1h atrás</time>
                     </div>
 
-                    <button title='Deletar comentário'>
+                    <button onClick={handleDeleteComment} title='Deletar comentário'>
                         <Trash size={24} />
                     </button>
                 </header>
